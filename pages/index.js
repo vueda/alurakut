b423cf0dd3a0cb0e89dff1edaacfc10b3bad54d7
Vinicius Ueda <vnsueda@gmail.com>
2021-07-17 01:10:58 -0300

    imersao react aula 4

--- pages/index.js
+++ pages/index.js
@@ -10,28 +10,41 @@ export async function getServerSideProps () {
   const githubUser = 'vueda'
   const res = await fetch(`https://api.github.com/users/${githubUser}/followers`)
   const followers = await res.json()
+
+  const resDato = await fetch('https://graphql.datocms.com/', {
+    method: 'POST',
+    headers: {
+      Authorization: process.env.DATOCMS_TOKEN,
+      'Content-Type': 'application/json',
+      Accept: 'application/json'
+    },
+    body: JSON.stringify({ query: 'query { allCommunities { id title imageUrl creatorSlug} }' })
+  })
+  const { data: { allCommunities } } = await resDato.json()
+
   return {
     props: {
       githubUser,
       followers: followers.map(f => ({ id: f.login, image: `https://github.com/${f.login}.png` })),
+      communities: allCommunities.map(f => ({ id: f.title, image: f.imageUrl, creatorSlug: f.creatorSlug })),
       stats: { confiavel: 3, legal: 2, sexy: 1 }
     }
   }
 }
 
-export default function Home ({ githubUser, stats, followers }) {
-  const [communities, setCommunities] = useState([{ id: 'Eu odeio acordar cedo', image: 'https://alurakut.vercel.app/capa-comunidade-01.jpg' }])
+export default function Home ({ githubUser, stats, followers, communities }) {
+  const [comm, setCommunities] = useState(communities)
   return (
     <AlurakutLayout
       user={githubUser}
       left={<ProfileBox user={githubUser} />}
       center={<>
         <WelcomeBox stats={stats} />
-        <CommunityForm communities={communities} setCommunities={setCommunities} />
+        <CommunityForm communities={comm} setCommunities={setCommunities} />
       </>}
       right={<>
         <RelationsBox title="Seguidores" relations={followers} />
-        <RelationsBox title="Comunidades" relations={communities} />
+        <RelationsBox title="Comunidades" relations={comm} />
       </>}
     />
   )
@@ -39,5 +52,6 @@ export default function Home ({ githubUser, stats, followers }) {
 Home.propTypes = {
   githubUser: PropTypes.string,
   stats: PropTypes.object,
-  followers: PropTypes.array
+  followers: PropTypes.array,
+  communities: PropTypes.array
 }
--- src/components/CommunityForm.js
+++ src/components/CommunityForm.js
@@ -5,11 +5,17 @@ import { Box } from './layout/Box'
 export function CommunityForm ({ communities, setCommunities }) {
   return (<Box>
         <h2 className="subTitle">Criar uma nova comunidade</h2>
-        <form onSubmit={(e) => {
+        <form onSubmit={async (e) => {
           e.preventDefault()
-          const id = e.target.title.value
-          const image = e.target.image.value
-          setCommunities([...communities, { id, image }])
+          const title = e.target.title.value
+          const imageUrl = e.target.image.value
+          const res = await fetch('/api/communities', {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({ title, imageUrl, creatorSlug: 'vueda' })
+          })
+          const data = await res.json()
+          setCommunities([...communities, { id: data.title, image: data.imageUrl }])
           e.target.reset()
         }}>
           <input name="title" type="text" placeholder="Qual vai ser o nome da sua comunidade?" aria-label="Qual vai ser o nome da sua comunidade?" />
